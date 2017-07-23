2ed42910acbdfa4fff9245067e283a4bd2594e5f
SadIF10 <30389687+SadIF10@users.noreply.github.com>
2017-07-23 23:51:09 +0600

    next stuff
    prototype done

---
+++ Assets/Scripts/character.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class character : MonoBehaviour {
+
+	public AudioSource sound;
+	public AudioClip a;
+	public AudioClip b;
+	public AudioClip c;
+	public AudioClip d;
+	int count = 41;
+	// Use this for initialization
+
+	void Start () {
+		Input.gyro.enabled = true;
+		playit ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.gyro.rotationRateUnbiased.y > 60) {
+			left ();
+		}
+	}
+
+	public void playit() {
+		if (count % 4 == 1) {
+			sound.clip = a;
+			sound.Play();
+		}
+		if (count % 4 == 2) {
+			sound.clip = b;
+			sound.Play ();
+		}
+		if (count % 4 == 3) {
+			sound.clip = c;
+			sound.Play();
+		}
+		if (count % 4 == 0) {
+			sound.clip = d;
+			sound.Play();
+		}
+	}
+
+	public void left() {
+		count--;
+		playit ();
+		transform.Rotate (0,90,0);
+	}
+
+	public void right() {
+		count++;
+		playit ();
+		transform.Rotate (0,-90,0);
+	}
+
+	public void menu() {
+		SceneManager.LoadScene ("menu");
+	}
+}
---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void abc() {
+		SceneManager.LoadScene ("ABC");
+	}
+
+	public void numbers() {
+		SceneManager.LoadScene("123");
+	}
+
+	public void rhymes() {
+
+	}
+
+	public void exit() {
+		Application.Quit ();
+	}
+}
